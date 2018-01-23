3348ccde85ebdde90c7a4bb2cbf477316424de75
Alex Moisi <amoisi@claritycon.com>
2018-01-22 16:22:57 -0800

    second

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-const TAX_RATE =0.08;
-const PHONE_PRICE =400;
-const ACCESSORY_PRICE=9.99;
-const SPENDING = 1750;
-
-function calcTax(price){
-    return price+(price*TAX_RATE);
+function add7(number) {
+    var add = number + 7;
+    return add;
 }
 
-function format(x){
+function input(){
     
-   return "$"+x.toFixed(2);
+    var userInput= prompt('Te rog adauga un numar: ');
+    userInput = parseInt(userInput);
+    //console.log(typeof(userInput));
+    //console.log(isNaN(userInput));
+    
+    
+    if (isNaN(userInput)){
+        return -1;
+    }
+    else{
+        return userInput;
+        
+    }
+    
 }
 
+var resultat =input();
 
-let remainingMoney =SPENDING;
-let totalCost =0;
-let phones=0;
-let accesory=0;
-
-while(remainingMoney > 0){
-        if (PHONE_PRICE<remainingMoney){
-            console.log("You bought a phone !" );
-            totalCost+=calcTax(PHONE_PRICE);
-            remainingMoney-=calcTax(PHONE_PRICE);
-            phones++;
-            if(ACCESSORY_PRICE<remainingMoney){
-                    console.log("You bought an accesory!" );
-                    totalCost+=calcTax(ACCESSORY_PRICE);
-                    remainingMoney-=calcTax(ACCESSORY_PRICE);
-                    accesory++;
-            }
-          console.log("Total Cost:" + format(totalCost));
-
-        }
-        else{
- 
-            break;}
-    }
-
-           console.log('That was it, you afforded '+ phones+' phones and '+ accesory +' accesories');
-           console.log('You\'re left with '+format(remainingMoney));
-
-// refactor to use less code. 
+if(resultat === -1){
+    alert('Nu putem lucra cu string');
+}
+else{
+    alert('Rezultatul adunarii cu 7 este: '+add7(resultat));
+}
